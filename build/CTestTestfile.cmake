1d92ee00bd6a3c452ab5d506b50ca58f62221c49
Arihant Lunawat <arihantb2@gmail.com>
2021-03-24 14:37:27 +0530

    Removed build files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arihant/ws/cpp_ws/src/graph_algorithms
-# Build directory: /home/arihant/ws/cpp_ws/src/graph_algorithms/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
